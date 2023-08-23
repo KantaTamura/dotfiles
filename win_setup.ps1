d0b9b311eea9520d08295085b952b53bf11b4cf0
KantaTamura <tkanta496@gmail.com>
2023-08-23 15:44:12 +0900

    add .gitconfig
    Signed-off-by: KantaTamura <tkanta496@gmail.com>

--- win_setup.ps1
+++ win_setup.ps1
@@ -1,4 +1,9 @@
+$home_dir = Get-ChildItem "." -File
 $configs = Get-ChildItem "./config" -File
+
+foreach ($home_conf in $home_dir) {
+    Copy-Item $home_conf.FullName -Destination "$env:USERPROFILE\$($home_conf.Name)"
+}
 
 foreach ($config in $configs) {
     Copy-Item $config.FullName -Destination "$env:USERPROFILE\.config\$($config.Name)"
