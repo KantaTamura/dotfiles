3f6e8273d914bd5a3bb3aa69bc7a3c617bdf67bd
KantaTamura <tkanta496@gmail.com>
2024-08-31 22:40:25 +0900

    nvim: add wsl-clipboard config
    Signed-off-by: KantaTamura <tkanta496@gmail.com>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,12 @@
-if not vim.g.vscode then
-    require("core/keymaps")
-    require("core/autocmd")
-    require("core/lazynvim")
-    require("core/options")
-else
+require("core/keymaps")
+require("core/autocmd")
+require("core/lazynvim")
+require("core/options")
+
+if vim.g.vscode then
     require("core/vscode")
+end
+
+if vim.fn.has("wsl") == 1 then
+    require("core/wsl-clipboard")
 end
