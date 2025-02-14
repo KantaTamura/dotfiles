54f97a478d4574f557187f8428820b47db6510a0
KantaTamura <tkanta496@gmail.com>
2025-02-15 04:06:12 +0900

    neovide: add Ctrl-Shift-V/C keybind
    Signed-off-by: KantaTamura <tkanta496@gmail.com>

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -9,6 +9,10 @@ if vim.g.vscode then
 	require("core/vscode")
 end
 
+if vim.g.neovide then
+	require("core/neovide")
+end
+
 if vim.fn.has("wsl") == 1 then
 	require("core/clipboard-wsl")
 elseif os.getenv "SSH_CLIENT" ~= nil or os.getenv "SSH_TTY" ~= nil then
